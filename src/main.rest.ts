80b9979ef471b380b44b4dfbbbdea492106e6e77
evgenygorchakov <stormdice23@gmail.com>
2024-06-01 21:37:40 +0500

    Add mongoose, setup db

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -5,12 +5,14 @@ import { RestApplication } from './rest/index.js';
 import { Config, RestConfig, RestSchema } from './shared/libs/config/index.js';
 import { Logger, PinoLogger } from './shared/libs/logger/index.js';
 import { Component } from './shared/types/index.js';
+import { MongoDatabaseClient, DatabaseClient } from './shared/libs/database-client/index.js';
 
 async function bootstrap() {
   const container = new Container();
   container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
   container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
   container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
+  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
 
   const app = container.get<RestApplication>(Component.RestApplication);
   await app.init();
--- src/shared/types/component.enum.ts
+++ src/shared/types/component.enum.ts
@@ -2,4 +2,5 @@ export const Component = {
   RestApplication: Symbol.for('RestApplication'),
   Config: Symbol.for('Config'),
   Logger: Symbol.for('Logger'),
+  DatabaseClient: Symbol.for('DatabaseClient'),
 } as const;
