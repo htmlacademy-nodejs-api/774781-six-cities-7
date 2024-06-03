046b5cb27377cf6883e31266b8cf1a5ccff01792
evgenygorchakov <stormdice23@gmail.com>
2024-06-03 11:52:22 +0500

    Update UserService, Separate app containers

--- src/shared/types/component.enum.ts
+++ src/shared/types/component.enum.ts
@@ -3,4 +3,6 @@ export const Component = {
   Config: Symbol.for('Config'),
   Logger: Symbol.for('Logger'),
   DatabaseClient: Symbol.for('DatabaseClient'),
+  UserService: Symbol.for('UserService'),
+  UserModel: Symbol.for('UserModel'),
 } as const;
