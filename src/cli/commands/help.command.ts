d6e4b8f9527abf041ece0216caa2391f6027c1df
evgenygorchakov <stormdice23@gmail.com>
2024-06-16 02:40:06 +0500

    Improve help command, clean up code

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -8,14 +8,21 @@ export class HelpCommand implements Command {
 
   public async execute(..._parameters: string[]): Promise<void> {
     console.info(`
-        ${chalk.yellow('Программа для подготовки данных для REST API сервера.')}
-        Пример:
-            cli.js --<command> [--arguments]
-        Команды:
-            --version:                   # выводит номер версии
-            --help:                      # печатает этот текст
-            --import <path>:             # импортирует данные из TSV
-            --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
+      ${chalk.yellow('A program for preparing data for the REST API of the server.')}
+
+      example:
+        cli.js --<command> [--arguments]
+
+      Available commands:
+        ${chalk.green('--version')}:         # output version
+        ${chalk.green('--help')}:            # output help
+        ${chalk.green('--import <path>')}:   # import data from tsv file,
+                                              <path> - path to text file
+
+        ${chalk.green('--generate <count> <path> <url>')}: # generates an arbitrary amount of text data.
+                                          <path> the path where the new file will be created.
+                                          <url> the server that the data will be generated from.
+                                          example: --generate 1 ./mocks/test-data.tsv http://localhost:3123/api
     `);
   }
 }
